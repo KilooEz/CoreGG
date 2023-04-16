f724d3ff91f2f7f0053dcfc107e610621675a625
Arskiz <96263232+KilooEz@users.noreply.github.com>
2023-04-16 22:22:50 +0300

    better

--- InternetRequest.lua
+++ InternetRequest.lua
@@ -1,4 +1,4 @@
-local Source = gg.makeRequest("https://raw.githubusercontent.com/KilooEz/CoreGG/main/CoreScript.lua").content
+local Source = gg.makeRequest("https://pastebin.com/raw/1wMArmLN").content
 
 if Source then
 pcall(load(Source))
